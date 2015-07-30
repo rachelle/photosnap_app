0710d3457b48255a71de6d84d588f865afe62e0b
Rachelle Herrera <rachelle.herera@gmail.com>
2015-07-30 12:41:27 -0700

    fixing indentations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :photo
 
   validates :content, presence: true
-  # validates :user_id, presence: true
-  # validates :photo_id, presence: true
+  
 end
